8719263c4c5eb2131a912a9389630c50ce1c77a0
Hildax <hilda1897@gmail.com>
2015-03-26 22:39:35 +0000

    c code

---
+++ FPGA_Client_for_Software_Allocator/C application code working with this fpga guy/software_system_memory_allocator.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xil_cache.h"
+
+#define CSTART 0
+#define NUMREQ 4
+#define RESULT 8
+#define COMMAND 12
+#define CONTENT 16
+#define READRESET 20
+#define LATBACK 24
+
+int FCORE_BASE[15] = {0x43C00000, 0x43C10000, 0x43C20000, 0x43C30000, 0x43C40000,
+			          0x43C50000, 0x43C60000, 0x43C00000, 0x43C80000, 0x43C90000,
+			          0x43CA0000, 0x43CB0000, 0x43CC0000, 0x43CD0000, 0x43CE0000};
+
+int req_count;
+int *pointer_array[100];
+int mf_counter, malloc_counter,free_counter;
+
+void print(char *str);
+
+void sys_alloc(void);
+
+void allocator(int num_req,int num_core);
+int check_clients(int i);
+
+int main()
+{
+	int num_client_working, req_each;
+	int i,j,tback;
+	char *str;
+    init_platform();
+    Xil_DCacheDisable();
+
+    /* ---------------------------------------------------------*/
+    num_client_working = 1;
+    /*----------------------------------------------------------*/
+    req_each = 8;
+
+    // tell each fpga client how many requests they need to perform
+    for(i = 0; i< num_client_working; i++)
+    	Xil_Out32(FCORE_BASE[i] + NUMREQ, req_each);
+
+    // start fpga clients
+    for(i = 0; i < num_client_working; i++)
+    	Xil_Out32(FCORE_BASE[i] + CSTART, 123);
+
+    // system allocator
+    allocator(req_each*num_client_working, num_client_working);
+
+    // reset read counter before reading out latencies
+    for(i = 0; i < num_client_working; i++)
+    	j = Xil_In32(FCORE_BASE[i] + READRESET);
+
+    // performance evaluation
+    for (j = 0; j<req_each; j++){
+    	for(i = 0; i <num_client_working; i++){
+			tback = Xil_In32(FCORE_BASE[i] + LATBACK);
+			xil_printf(" %d\r", tback);
+		}
+    	xil_printf("\n", tback);
+    }
+	return 0;
+
+}
+
+void allocator(int num_req,int num_core){
+
+	int cmd;
+	int client_id;
+	int content;
+
+	int res;
+
+	req_count = 0;
+	client_id = 0;
+
+	cmd = check_clients(client_id);
+
+	while(req_count < num_req){
+
+		cmd = check_clients(client_id);
+		while(cmd == 0){
+			if(client_id == num_core - 1){ client_id = 0;}
+			else{ client_id++; }
+			cmd = check_clients(client_id);
+		}
+		// read request content
+		content =  Xil_In32(FCORE_BASE[client_id] + CONTENT);
+
+		if(cmd == 1){
+			pointer_array[malloc_counter] = (char*)malloc(content);
+			Xil_Out32(FCORE_BASE[client_id] + RESULT, &pointer_array[malloc_counter]);
+			//res = &pointer_array[malloc_counter];
+			malloc_counter ++;
+		}else if(cmd ==2 ){
+			free(pointer_array[free_counter]);
+			Xil_Out32(FCORE_BASE[client_id] + RESULT, 1111);
+			if(client_id == num_core - 1){ client_id = 0;}
+			else{ client_id++; }
+			//res = &pointer_array[free_counter];
+			free_counter++;
+		}
+
+		//xil_printf(" %d %d %d %d\r\n", req_count, cmd, content, res);
+		cmd = 0;
+		req_count++;
+	}
+}
+
+int check_clients(int i){
+	return Xil_In32(FCORE_BASE[i] + COMMAND);
+}
